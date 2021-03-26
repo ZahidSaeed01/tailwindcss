814998f7640c3df8b0f89acfa44e0ba59779c899
Zahid Saeed <muhammadzahid1099@gmail.com>
2021-03-27 00:59:43 +0500

    basic structure tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="h-64">
+
+      <div class="p-4 m-4 bg-green-600">
+        <h1 class="text-2xl text-white font-bold"> Tailwind CSS demo</h1>
+      </div>
+
+      <div class="p-4 m-4 bg-green-300 h-full">
+        <h2 class="text-red-600">Have fun using tailwind in the react</h2>
+        <button class="bg-blue-500 hover:bg-blue-900 text-white py-2 px-2 rounded"> Click ME</button>
+      </div>
+
     </div>
   );
 }
