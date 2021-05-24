2e64e0f9c040844703ac177596463ae0827bd04e
Zahid Saeed <muhammadzahid1099@gmail.com>
2021-05-24 18:50:19 +0500

    untract generated tailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import './styles/main.css';
-
+import './styles/tailwindOutput.css';
+import Header from './Components/Header'
 function App() {
   return (
     <div class="h-64">
-
-      <div class="p-4 m-4 bg-green-600">
-        <h1 class="text-2xl text-white font-bold"> Tailwind CSS demo</h1>
-      </div>
-
+      <Header />
       <div class="p-4 m-4 bg-green-300 h-full">
         <h2 class="text-red-600">Have fun using tailwind in the react</h2>
         <button class="bg-blue-500 hover:bg-blue-900 text-white py-2 px-2 rounded"> Click ME</button>
